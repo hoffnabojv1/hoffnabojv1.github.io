b03c2eb61fe89d3ce42bf2b84673af6e509acea7
hoffnabojv1 <hoffnaboj@interia.pl>
2024-12-16 09:52:31 +0100

    Task import/export

--- script.js
+++ script.js
@@ -20,3 +20,47 @@ async function init(){
         storage.malgosia = {}
     }
 }
+
+function JSONtoFile(json, filename){
+    let blob = new Blob([JSON.stringify(json)], {type: 'application/json'});
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+}
+
+function saveusedtasks(){
+    JSONtoFile(storage.tasks_used, "tasks_used.json")
+}
+
+function filetoJSON(file){
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = function(){
+            resolve(JSON.parse(reader.result))
+        }
+        reader.readAsText(file)
+    })
+}
+
+function orjson(a, b){
+    let c = {}
+    for(let i in a){
+        c[i] = a[i] || b[i]
+    }
+    return c
+}
+
+function loadusedtasks(){
+    let input = document.createElement("input")
+    input.type = "file"
+    input.accept = ".json"
+    input.onchange = async function(){
+        let file = input.files[0]
+        let json = await filetoJSON(file)
+        storage.tasks_used = orjson(storage.tasks_used, json)
+        alert("Wczytano plik")
+    }
+    input.click()
+}
